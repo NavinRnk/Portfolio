85a94f0137a3813b9dad47e6304b308ac5ee525c
kqcloudsrv\naveenkumar.r <naveenkumar.r@knowledgeq.com>
2024-05-14 18:18:19 +0530

    images updated

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,11 +9,13 @@ export default function AboutMe() {
           <p className="section--title">About</p>
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            <ul>
-              <li>I'm NaveenKumar R, a skilled Full Stack Web Developer renowned for crafting stunning websites with precision and elegance. My expertise lies in leveraging cutting-edge technologies such as React.js, SCSS, and Bootstrap to create visually appealing and seamlessly functional web experiences.</li>
-              <li>In the realm of Frontend development, I excel in utilizing tools like React.js, Redux, Redux Toolkit, Axios, SaSS, and CSS3 to bring designs to life with finesse and efficiency.</li>
-              <li> On the Backend front, I am proficient in Node.js and Express.js, adept at building robust server-side applications. My knowledge extends to database management with MySQL, ensuring optimal data handling and retrieval.</li>
-            </ul>
+            I'm NaveenKumar R, a skilled Full Stack Web Developer renowned for crafting stunning websites with precision and elegance. My expertise lies in leveraging cutting-edge technologies such as React.js, SCSS, and Bootstrap to create visually appealing and seamlessly functional web experiences.
+          </p>
+          <p className="hero--section-description">
+            In the realm of Frontend development, I excel in utilizing tools like React.js, Redux, Redux Toolkit, Axios, SaSS, and CSS3 to bring designs to life with finesse and efficiency.
+          </p>
+          <p className="hero--section-description">
+            On the Backend front, I am proficient in Node.js and Express.js, adept at building robust server-side applications. My knowledge extends to database management with MySQL, ensuring optimal data handling and retrieval.
           </p>
           <p className="hero--section-description">
             Currently, I am honing my skills and gaining practical experience as a Junior Software Developer at KnowledgeQ Interactive Consultancy Pvt Ltd Coimbatore since 2023. In this role, I actively contribute to the development of innovative solutions while continuously expanding my expertise in various technologies.
