dc9ceeb057e7058c72305b3d66a2da3e0afef29f
kqcloudsrv\naveenkumar.r <naveenkumar.r@knowledgeq.com>
2024-06-18 11:43:12 +0530

    Latest updated commit

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import HeroBgAnimation from '../HeroBgAnimation'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, SocialMediaIcons, SocialMediaIcon, ResumeButton } from './HeroStyle'
+import HeroImg from '../../images/HeroImage.jpg'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/constants';
+
+const HeroSection = () => {
+
+    const handleDownloadResume = () => {
+        const downloadLink = document.createElement('a');
+        downloadLink.href = '/assets/NaveenKumar R.pdf'; // Path to your PDF file
+        downloadLink.download = 'NaveenKumar R.pdf'; // Name of the downloaded file
+        downloadLink.click();
+    };
+
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>Hi, I am <br /> {Bio.name}</Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>{Bio.description}</SubTitle>
+                        <ResumeButton onClick={handleDownloadResume} target='display'>Download Resume</ResumeButton>
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+
+                        <Img src={HeroImg} alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection
